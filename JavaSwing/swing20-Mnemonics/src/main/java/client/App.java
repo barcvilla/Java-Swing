29aeaf57c2104e0730789c47669fec26d1e6bfb1
Carlos Villanueva <ceva_19@hotmail.com>
2018-10-22 19:27:24 -0300

    Building GUI Apps - Using Swing

---
+++ JavaSwing/swing20-Mnemonics/src/main/java/client/App.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author PC
+ */
+public class App {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainFrame();
+            }
+        });
+    }
+}
