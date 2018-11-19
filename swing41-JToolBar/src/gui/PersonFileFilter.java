e424212c54d31c8af9a731ce5cab24161b061e78
Carlos Villanueva <ceva_19@hotmail.com>
2018-11-19 11:52:18 -0300

    Building Java Swing App

---
+++ swing41-JToolBar/src/gui/PersonFileFilter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ *
+ * @author PC
+ */
+public class PersonFileFilter extends FileFilter{
+
+    @Override
+    public boolean accept(File pathname) {
+        if(pathname.isDirectory())
+        {
+            return true;
+        }
+        
+        String name = pathname.getName();
+        String extension = Utils.getFileExtension(name);
+        if(extension == null)
+        {
+            return false;
+        }
+        
+        if(extension == "per")
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Person database files (*.per)";
+    }
+}
