22a8241f97b7b5ee146f9a1a8bbe2886221333f5
Carlos Villanueva <ceva_19@hotmail.com>
2018-11-20 10:54:26 -0300

    Building Java Swing App

---
+++ JavaSwing46-DataWithTreeNodes/src/gui/MessagePanel.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
+
+/**
+ *
+ * @author PC
+ */
+public class MessagePanel extends JPanel{
+    
+    private JTree serverTree;
+    
+    public MessagePanel()
+    {
+        serverTree = new JTree(createTree());
+        //set events
+        serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+        serverTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                //obtenemos el nodo
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode)serverTree.getLastSelectedPathComponent();
+                //obtenemos el nombre
+                Object userObject = node.getUserObject();
+                System.out.println(userObject);
+            }
+        });
+        
+        setLayout(new BorderLayout());
+        add(new JScrollPane(serverTree), BorderLayout.CENTER);
+    }
+    
+    private DefaultMutableTreeNode createTree()
+    {
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Servers");
+        // creamos la jerarquia
+        DefaultMutableTreeNode branch1 = new DefaultMutableTreeNode("USA");
+        DefaultMutableTreeNode server1 = new DefaultMutableTreeNode("New York");
+        DefaultMutableTreeNode server2 = new DefaultMutableTreeNode("Boston");
+        DefaultMutableTreeNode server3 = new DefaultMutableTreeNode("Los Angeles");
+        
+        branch1.add(server1);
+        branch1.add(server2);
+        branch1.add(server3);
+        
+        DefaultMutableTreeNode branch2 = new DefaultMutableTreeNode("UK");
+        DefaultMutableTreeNode server4 = new DefaultMutableTreeNode("London");
+        DefaultMutableTreeNode server5 = new DefaultMutableTreeNode("Edinburgh");
+        
+        branch2.add(server4);
+        branch2.add(server5);
+        
+        top.add(branch1);
+        top.add(branch2);
+        
+        return top;
+    }
+}
