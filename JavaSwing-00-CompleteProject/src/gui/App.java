8b296ad0d5d5f0896ef778aa1e9ccde6940468ae
Carlos Villanueva <ceva_19@hotmail.com>
2019-01-07 13:17:42 -0300

    Building Java Swing Application

---
+++ JavaSwing-00-CompleteProject/src/gui/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.SwingUtilities;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MainFrame();
+			}
+		});	
+	}
+
+}
